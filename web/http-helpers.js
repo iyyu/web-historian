5d1bb4c5b919ba65c068d53b71ea7fd929e02839
iyyu <iyucomms@gmail.com>
2017-12-20 15:49:57 -0800

    Get method solo work

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,8 +14,36 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  fs.readFile(archive.paths.siteAssets + asset, 'utf8', (err, data) => {
+    if (err) {
+      console.log('ASSET: ', asset); 
+      throw err; 
+    }
+    console.log(data); 
+  });
+  
+  
 };
 
 
 
 // As you progress, keep thinking about what helper functions you can put here!
+module.exports.prepareResponse = function(req, cb) {
+  var data = '';
+  req.on('data', function(chunk) { data += chunk; });
+  req.on('end', function() { cb(data); });
+};
+
+module.exports.respond = function(res, data, status = 200) {
+  res.writeHead(status, headers);
+  res.end(JSON.stringify(data));
+};
+
+module.exports.send404 = function(res) {
+  exports.respond(res, 'Not Found', 404);
+};
+
+module.exports.redirector = function(res, loc, status = 302) {
+  res.writeHead(status, { Location: loc });
+  res.end();
+};
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,24 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var utils = require('./http-helpers');
+var url = require('url');
 
 exports.handleRequest = function (req, res) {
+  // if req.method === "GET"
+    // check for index.html
+  //call serveAssets which will either 200 or 404
+  console.log('method: ' + req.method + ' and URL: ' + req.url);
+  if (req.method === 'GET') {
+    // console.log('Req method registered');
+    if (req.url === '/' || req.url === '/index.html') {
+      // console.log('Req URL received');
+      utils.serveAssets(res, '/index.html', function(err, data) {
+        console.log(data); 
+      } ); 
+    }
+  }
+
+
   res.end(archive.paths.list);
 };
